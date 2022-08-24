5c5d506465266f68144af3366156cd9908a71aab
OwlHerring <mark@mora-fretheim.com>
2022-08-24 21:56:25 +0900

    Add ability to alter both display value and string

--- resources/script.js
+++ resources/script.js
@@ -1,9 +1,10 @@
 let displayValue = 0;
+let displayValueString = String(displayValue);
 
 // assigning functionality to the numeral buttons:
-const numeralButtonsTemp = document.querySelectorAll(".numerals button");
+//const numeralButtonsTemp = document.querySelectorAll(".numerals button");
 const numeralButtons = []; 
-numeralButtonsTemp.forEach((button) => {
+document.querySelectorAll(".numerals button").forEach((button) => {
     let num = Number(button.textContent);
     console.log(num);
     if(isNaN(num)) {
@@ -13,27 +14,41 @@ numeralButtonsTemp.forEach((button) => {
     else{
         numeralButtons[num] = button;
         numeralButtons[num].addEventListener('click', () => {
-            displayValue = num;
-            printDisplayValue();
+            alterDisplayValue(num);
             //alert("It works!")
         });
     }
 })
 
+// The thing that will separate this from newDisplayValue(0) is that it will
+// clear the board of all operands.
 function clearField(){
-    displayValue = 0;
+    newDisplayValue(0);
+}
+
+// this function is for adding a numeral (or decimal??) to displayValue and its string.
+function alterDisplayValue(num){
+    if(displayValueString === "0") displayValueString = String(num);
+    else displayValueString += num;
+    displayValue = Number(displayValueString);
+    printDisplayValue();
+}
+// this function is for completely changing the displayValue and displayValueString.
+function newDisplayValue(num){
+    displayValue = num;
+    displayValueString = String(displayValue);
     printDisplayValue();
 }
 
 function printDisplayValue(){
-    console.log(displayValue);
+    console.log(displayValueString);
 }
 
 function operate(op, a, b){
     switch(op){
         case "+": return a + b;
         case "-": return a - b;
-        case "*": return a * b;
+        case "x": return a * b;
         case "/": return a / b;
     }
 }
