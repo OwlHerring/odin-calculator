125138c0a30e4e8eaf814578904525ff7e87bb85
OwlHerring <mark@mora-fretheim.com>
2022-08-24 21:36:36 +0900

    Assign functions to the numeral buttons and clear

--- resources/script.js
+++ resources/script.js
@@ -1,7 +1,24 @@
-//function add(a,b){ return a + b; }
-//function subtract(a,b){ return a - b; }
-//function multiply(a,b){ return a * b; }
-//function divide(a,b){ return a / b;}
+let displayValue = 0;
+
+// assigning functionality to the numeral buttons:
+const numeralButtonsTemp = document.querySelectorAll(".numerals button");
+const numeralButtons = []; 
+numeralButtonsTemp.forEach((button) => {
+    let num = Number(button.textContent);
+    console.log(num);
+    if(isNaN(num)) {
+        button.addEventListener('click', clearField);
+        return;
+    }
+    else{
+        numeralButtons[num] = button;
+        numeralButtons[num].addEventListener('click', () => alert("It works!"));
+    }
+})
+
+function clearField(){
+    console.log("Clear doesn't work yet.");
+}
 
 function operate(op, a, b){
     switch(op){
