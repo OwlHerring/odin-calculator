6e272c3900ff29ddcb8bf2266a321da52ca722d3
OwlHerring <mark@mora-fretheim.com>
2022-08-24 22:28:53 +0900

    Change displayValue into an array
    I also changed displayValueString into an array, and made changes to
    the code accordingly.
    
    The reason is to accommodate for inputting multiple numbers separated by
    operands (which will be numbered in the same way as the numbers)

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,19 @@
-let displayValue = 0;
-let displayValueString = String(displayValue);
+// initializing as [] mostly just as a reminder for myself.
+// however, their initial states are set to [0] and ["0"] by clearField().
+let displayValue = [];
+let displayValueString = [];
+// This 
+let operandString = [];
 
 // this will set to true whenever the decimal button has been pressed.
 // If true, the decimal button will not have any function if pressed again.
 // Set this back to false whenever an operand has been pressed or it's reset
 let decimalFunctionOn = false;
+// this is what determines which displayValue[] we're on, basically.
+let numberCount = 0;
+
+
+clearField();
 
 // assigning functionality to the numeral buttons:
 //const numeralButtonsTemp = document.querySelectorAll(".numerals button");
@@ -33,8 +42,12 @@ document.querySelectorAll(".numerals button").forEach((button) => {
 // The thing that will separate this from newDisplayValue(0) is that it will
 // clear the board of all operands.
 function clearField(){
-    newDisplayValue(0);
+    displayValue = [0];
+    displayValueString = [String(displayValue[0])];
+
+    //newDisplayValue(0);
     decimalFunctionOn = false;
+    printDisplayValue();
 }
 
 function enableDecimal(){
@@ -47,20 +60,20 @@ function enableDecimal(){
 
 // this function is for adding a numeral (or decimal??) to displayValue and its string.
 function alterDisplayValue(num){
-    if(displayValueString === "0") displayValueString = String(num);
-    else displayValueString += num;
-    displayValue = Number(displayValueString);
+    if(displayValueString[numberCount] === "0") displayValueString[numberCount] = String(num);
+    else displayValueString[numberCount] += num;
+    displayValue[numberCount] = Number(displayValueString[numberCount]);
     printDisplayValue();
 }
 // this function is for completely changing the displayValue and displayValueString.
-function newDisplayValue(num){
-    displayValue = num;
-    displayValueString = String(displayValue);
-    printDisplayValue();
-}
+//function newDisplayValue(num){
+//    displayValue = num;
+//    displayValueString = String(displayValue);
+//    printDisplayValue();
+//}
 
 function printDisplayValue(){
-    console.log(displayValueString);
+    console.log(displayValueString[numberCount]);
 }
 
 function operate(op, a, b){
